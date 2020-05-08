72eff91eccfe716321933a4ebe9ef1e2d8d4e31f
jmassuia <massuia1507@gmail.com>
2020-05-08 00:39:02 -0300

    Upload images function

--- website/src/pages/Home/index.js
+++ website/src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 
+import {Card,Button, Form} from 'react-bootstrap'
 import logo from '../../Assets/Caritas_Brand.png';
 import './styles.css';
 
 export default function Home(){
     return (
-    <h1>Pagina Principal</h1>
+        <h1>pagina principal</h1>
     )
 }
