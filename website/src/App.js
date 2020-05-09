9612bdfea4fe112e04f4498ec1eb64a1a0fe9593
10DECOPAS <guilherme.oliveira.ipms@gmail.com>
2020-05-09 19:43:18 -0300

    home_pages

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './routes'
 import './Global.css';
-
+import 'mdbreact/dist/css/mdb.css';
 function App() {
   //Componentes que devem estar presentes em toda as paginas deveram ser colocados aqui
   return (
